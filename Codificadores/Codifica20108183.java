bd30ad1461655877dea0ffe8bda08332fb6a78fa
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-03-23 11:02:51 -0300

    ADD método para decodificar a string

--- Codificadores/Codifica20108183.java
+++ Codificadores/Codifica20108183.java
@@ -33,8 +33,30 @@ public class Codifica20108183 implements Codifica {
         return textoCodificado;
     }
 
-    public String decodifica(String str) {
-        return str;
+    public String decodifica(String textoADecodificar) {
+        String textoDecodificado = "";
+        // Transforma em maiusculo para poder comparar com o alfabeto
+        String textoEmMaiusculo = textoADecodificar.toUpperCase(); 
+        for (int i = 0; i < textoEmMaiusculo.length(); i++) {
+            char caractereAtual = textoEmMaiusculo.charAt(i);
+            if (Character.isDigit(caractereAtual)) {
+                // Caso seja um digito, ou seja, 
+                //não está no alfabeto, apenas insere
+                textoDecodificado += caractereAtual;
+            } else {
+                for (int j = 0; i < alfabeto.length(); j++) {
+                    char letraAtual = alfabeto.charAt(j);
+                    if (letraAtual == caractereAtual) {
+                        // Pega a posição avançando tres indices
+                        // Utiliza-se o floorMod para não sair do range do alfabeto
+                        int posicaoCodificada = Math.floorMod((i + deslocamento), alfabeto.length());
+                        textoDecodificado += alfabeto.charAt(posicaoCodificada);
+                        break;
+                    }
+                }
+            }
+        }
+        return textoDecodificado;
     }
 
     public String getNomeAutor() {
