5c46baa77a978ea42ed21bd4edf2fc1a5bf373b4
Guilherme Vieira <guilherme.camara@edu.pucrs.br>
2021-03-24 18:20:30 -0300

    melhorado algoritmo

--- Codificadores/Codifica18103690.java
+++ Codificadores/Codifica18103690.java
@@ -1,18 +1,52 @@
 package Codificadores;
 
 public class Codifica18103690 implements Codifica {
-	//private String tabela = ;
+    private static String tabela = "zyxwvutsrqponmlkjihgfedcbaç0987654321ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static int deslc = 5;
 
     // Recebe um string e retorna o correspondente codificado
     public String codifica(String str){
-		// todo
-        return new StringBuilder(str).reverse().toString();
+        String codificada = "";
+        for (char s : str.toCharArray()) {
+            if (!Character.isLetterOrDigit(s)) {
+                codificada += s;
+            } else {
+				// deslocamento
+                for (int i = 0; i < tabela.length(); i++) {
+                    if (tabela.charAt(i) == s) {
+                        int pos = Math.floorMod((i+deslc), tabela.length());
+                        codificada += tabela.charAt(pos);
+                    }
+                }
+			}
+		}
+
+		codificada = new StringBuilder(codificada).reverse().toString();
+
+        return codificada;
 	}
 
     // Recebe um string codificado e retorna o correspondente decodificado
     public String decodifica(String str){
-		// todo
-        return new StringBuilder(str).reverse().toString();
+        String decodificada = new StringBuilder(str).reverse().toString();
+
+		char[] s = decodificada.toCharArray();
+
+        for (int indexSub=0; indexSub<decodificada.length(); indexSub++){
+            if (!Character.isLetterOrDigit(s[indexSub])) {
+                // se for igual mantem como esta
+            } else {
+				// deslocamento
+                for (int i = 0; i < tabela.length(); i++) {
+                    if (tabela.charAt(i) == s[indexSub]) {
+                        int pos = Math.floorMod((i-deslc), tabela.length());
+                        s[indexSub] = tabela.charAt(pos);
+                    }
+                }
+            }
+        }
+		decodificada = new String(s);
+        return decodificada;
 	}
 
 	public String getMatriculaAutor(){
