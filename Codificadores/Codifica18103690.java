935c8c1a9f1b8d1ecbfbd1fc967beb23cf239c9d
Guilherme Vieira <guilherme.camara@edu.pucrs.br>
2021-03-24 18:39:06 -0300

    melhorado algoritmo e trocada a tabela de substituição

--- Codificadores/Codifica18103690.java
+++ Codificadores/Codifica18103690.java
@@ -1,23 +1,18 @@
 package Codificadores;
 
 public class Codifica18103690 implements Codifica {
-    private static String tabela = "zyxwvutsrqponmlkjihgfedcbaç0987654321ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static int deslc = 5;
+    private static String tabela = "ã zyxwvutsrqponmlkjihgfedcba:ç0987654321úABCDEFGHIJKLMNOPQRSTUVWXYZõâõô";
+    private static int deslc = 8;
 
     // Recebe um string e retorna o correspondente codificado
     public String codifica(String str){
         String codificada = "";
-        for (char s : str.toCharArray()) {
-            if (!Character.isLetterOrDigit(s)) {
-                codificada += s;
-            } else {
-				// deslocamento
-                for (int i = 0; i < tabela.length(); i++) {
-                    if (tabela.charAt(i) == s) {
-                        int pos = Math.floorMod((i+deslc), tabela.length());
-                        codificada += tabela.charAt(pos);
-                    }
-                }
+		for (char s : str.toCharArray()) {
+			for (int i = 0; i < tabela.length(); i++) {
+				if (tabela.charAt(i) == s) {
+					int pos = Math.floorMod((i+deslc), tabela.length());
+					codificada += tabela.charAt(pos);
+				}
 			}
 		}
 
@@ -32,19 +27,14 @@ public class Codifica18103690 implements Codifica {
 
 		char[] s = decodificada.toCharArray();
 
-        for (int indexSub=0; indexSub<decodificada.length(); indexSub++){
-            if (!Character.isLetterOrDigit(s[indexSub])) {
-                // se for igual mantem como esta
-            } else {
-				// deslocamento
-                for (int i = 0; i < tabela.length(); i++) {
-                    if (tabela.charAt(i) == s[indexSub]) {
-                        int pos = Math.floorMod((i-deslc), tabela.length());
-                        s[indexSub] = tabela.charAt(pos);
-                    }
-                }
-            }
-        }
+		for (int indexSub=0; indexSub<decodificada.length(); indexSub++){
+			for (int i = 0; i < tabela.length(); i++) {
+				if (tabela.charAt(i) == s[indexSub]) {
+					int pos = Math.floorMod((i-deslc), tabela.length());
+					s[indexSub] = tabela.charAt(pos);
+				}
+			}
+		}
 		decodificada = new String(s);
         return decodificada;
 	}
